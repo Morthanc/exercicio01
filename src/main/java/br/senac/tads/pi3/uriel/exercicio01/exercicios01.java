847de4b39a8500f65cb49ce35b57c4c47840ba6a
uriel.pgoliveira <uriel.pgoliveira@CAS419199C14707.senacsp.edu.br>
2015-08-14 20:52:47 -0300

    commit1

---
+++ src/main/java/br/senac/tads/pi3/uriel/exercicio01/exercicios01.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.uriel.exercicio01;
+
+/**
+ *
+ * @author uriel.pgoliveira
+ */
+public class exercicios01 {
+    public static void main(String args[]){
+        System.out.println("Uriel");
+                
+        
+    }
+}
