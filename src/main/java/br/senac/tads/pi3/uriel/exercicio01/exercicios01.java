e9c1dd7beb9671bae3fabb19a10db46426cd3f39
uriel.pgoliveira <uriel.pgoliveira@CAS419199C14707.senacsp.edu.br>
2015-08-14 22:07:25 -0300

    Cdastro 1

--- src/main/java/br/senac/tads/pi3/uriel/exercicio01/exercicios01.java
+++ src/main/java/br/senac/tads/pi3/uriel/exercicio01/exercicios01.java
@@ -4,14 +4,46 @@
  * and open the template in the editor.
  */
 package br.senac.tads.pi3.uriel.exercicio01;
-
+import java.util.Scanner;
 /**
  *
  * @author uriel.pgoliveira
  */
 public class exercicios01 {
+    public static Scanner teclado = new Scanner(System.in);
+    
     public static void main(String args[]){
-        System.out.println("Uriel");
+       String nome,email,telefone,dataNasct;
+       byte cod=0;
+       
+       
+       
+       do {
+           
+           System.out.println("Deseja realizar o cadastro?\n"
+                   + "Digite 1 para sim\n"
+                   + "Digite 2 para sair\n");
+           cod = teclado.nextByte();
+           
+           
+           
+            System.out.println("Digite seu nome: ");
+            nome = teclado.nextLine();
+            teclado.next();
+            
+            System.out.println("Digite seu email: ");
+            email = teclado.nextLine();
+            teclado.next();
+            
+            System.out.println("Digite seu telefone: ");
+            telefone = teclado.nextLine();
+            teclado.next();
+            
+            System.out.println("Digite a data de nascimento (DD/MM/AA): ");
+            dataNasct = teclado.nextLine();
+            teclado.next();
+            
+       } while(cod!=2);
                 
         
     }
